9318a90e221c72fc6b5f6dd17443a8500f3dc222
nedasv <nedasvauspas@gmail.com>
2025-11-11 23:10:27 +0000

    diff test data

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -2,4 +2,4 @@
 
 USE berties_books;
 
-INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99), ('Phils Poetry', 67.69), ('Toms Trails', 19.99) ;
