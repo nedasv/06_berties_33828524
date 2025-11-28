87e4f45f9835b73c94bf4083fee09d0b8fa52dad
nedasv <nedasvauspas@gmail.com>
2025-11-28 00:19:29 +0000

    task 4

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,8 @@
 const express = require("express")
 const router = express.Router()
 
+const { check, validationResult } = require('express-validator');
+
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
       res.redirect('../users/login') // redirect to the login page
@@ -14,17 +16,24 @@ router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
-router.get('/search-result', function (req, res, next) {
-     //searching in the database
-    let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.search_text + "%'"; // query database to get all books with name that contains the keyword using "like"
-    console.log(sqlquery)
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        res.render("searchresult.ejs", {availableBooks: result})
-    });
+router.get('/search-result', [check('search_text').notEmpty()], function (req, res, next) {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.render("search.ejs")
+    }
+    else {
+        //searching in the database
+        let sqlquery = "SELECT * FROM books WHERE name LIKE '%" + req.query.search_text + "%'"; // query database to get all books with name that contains the keyword using "like"
+        console.log(sqlquery)
+        // execute sql query
+        db.query(sqlquery, (err, result) => {
+            if (err) {
+                next(err)
+            }
+            res.render("searchresult.ejs", {availableBooks: result})
+        });
+    }
 });
 
 router.get('/list', function(req, res, next) {
@@ -42,18 +51,24 @@ router.get('/addbook', redirectLogin, function(req, res, next){
     res.render('addbook.ejs')
 });
 
-router.post('/bookadded', function (req, res, next) {
-    // saving data in database
-    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
-    // execute sql query
-    let newrecord = [req.body.name, req.body.price]
-    db.query(sqlquery, newrecord, (err, result) => {
-        if (err) {
-            next(err)
-        }
-        else
-            res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
-    })
+router.post('/bookadded', [check('name').notEmpty().isLength({min: 1, max: 100}), check('price').isFloat({min: 0})], function (req, res, next) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.render('addbook.ejs')
+    }
+    else {
+        // saving data in database
+        let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+        // execute sql query
+        let newrecord = [req.body.name, req.body.price]
+        db.query(sqlquery, newrecord, (err, result) => {
+            if (err) {
+                next(err)
+            }
+            else
+                res.send(' This book is added to database, name: '+ req.body.name + ' price '+ req.body.price)
+        })
+    }
 }) 
 
 router.get('/bargainbooks',function(req, res, next){
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const bcrypt = require('bcrypt')
 const saltRounds = 10
 
+const { check, validationResult } = require('express-validator');
+
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
       res.redirect('./login') // redirect to the login page
@@ -20,8 +22,14 @@ router.get('/login', function (req, res, next) {
     res.render('login.ejs')
 })
 
-router.post('/loggedin', function (req, res, next) {
-    let sqlquery = "SELECT password_hash FROM users WHERE username='" + req.body.username + "'"; // query database to get all the books
+router.post('/loggedin', [check('username').notEmpty(), check('password').notEmpty()], function (req, res, next) {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        res.render('login.ejs')
+    }
+    else {
+        let sqlquery = "SELECT password_hash FROM users WHERE username='" + req.body.username + "'"; // query database to get all the books
     // execute sql query
     db.query(sqlquery, (err, result) => {
         if (err) {
@@ -50,6 +58,7 @@ router.post('/loggedin', function (req, res, next) {
             })
         }
     });
+    }
 })
 
 router.get('/logout', redirectLogin, (req,res) => {
@@ -72,23 +81,31 @@ router.get('/audit', redirectLogin, function (req, res, next) {
     });
 })
 
-router.post('/registered', function (req, res, next) {
-    // saving data in database
-    const plainPassword = req.body.password
-    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
-        // Store hashed password in your database.
-        let sqlquery = "INSERT INTO users (username, first_name, last_name, email, password_hash) VALUES (?,?,?,?,?)"
-        // execute sql query
-        let newrecord = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword]
-        db.query(sqlquery, newrecord, (err, result) => {
-            if (err) {
-                next(err)
-            }
-            else
-                result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email + 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
-                res.send(result)
-        })
-    })                                                                          
+router.post('/registered', 
+    [check('email').isEmail(), check('username').isLength({ min: 5, max: 20}), check('password').isLength({min: 8})], 
+    function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.render('./register')
+    }
+    else { 
+        // saving data in database
+        const plainPassword = req.body.password
+        bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+            // Store hashed password in your database.
+            let sqlquery = "INSERT INTO users (username, first_name, last_name, email, password_hash) VALUES (?,?,?,?,?)"
+            // execute sql query
+            let newrecord = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword]
+            db.query(sqlquery, newrecord, (err, result) => {
+                if (err) {
+                    next(err)
+                }
+                else
+                    result = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email + 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
+                    res.send(result)
+            })
+        })         
+    }                                                                 
 }); 
 
 router.get('/list', redirectLogin, function (req, res, next) {
